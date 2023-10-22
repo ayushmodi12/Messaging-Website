317b03e2af8bd48996ce23473d209cca51bd8597
ayushmodi12 <modiayush@iitgn.ac.in>
2023-10-22 15:06:57 +0530

    optimize

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,14 +5,17 @@ import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import { ChakraProvider } from "@chakra-ui/react";
 import ChatProvider from "./Context/ChatProvider";
+import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(
-  <BrowserRouter>
-    <ChakraProvider>
+  <ChakraProvider>
+    <BrowserRouter>
       <ChatProvider>
         <App />
       </ChatProvider>
-    </ChakraProvider>
-  </BrowserRouter>,
+    </BrowserRouter>
+  </ChakraProvider>,
   document.getElementById("root")
 );
+
+reportWebVitals(console.log);
